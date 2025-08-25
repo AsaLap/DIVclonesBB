8469b3e81aa4f1f082e3fd64d742926edb4204cb
AsaLap <asalaporte@gmail.com>
2025-08-25 15:44:47 +0200

    Automatically erasing previous pca concat

--- pipeline_kmer_to_loadings.sh
+++ pipeline_kmer_to_loadings.sh
@@ -154,6 +154,7 @@ else
 fi
 
 if "$do_concat";then
+  touch "$directory_output"/"$name"_pca_selection_"$split_factor"_chunks_"$n_loadings"_loadings_"$n_components"_components.tsv
   tail -q -n +2 "$directory_output"/*"$n_loadings"_pca_loadings_"$n_components"_components.tsv >> "$directory_output"/"$name"_pca_selection_"$split_factor"_chunks_"$n_loadings"_loadings_"$n_components"_components.tsv
 #  str(n_loadings) + "_pca_loadings_" + str(n_components) + "_components.tsv"
   echo "Global PCA matrix: ${name}_pca_selection_${split_factor}_chunks_${n_loadings}_loadings_${n_components}_components.tsv"
